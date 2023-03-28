c23c86320da1ac31d418038d5bb59cf95e0166a1
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-28 16:58:01 +0400

    Master task(change)

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,15 +3,11 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
-        if (first > 0 && second > 0) {
-            first += second;
-            second = first - second;
-            first = first - second;
-            System.out.println(first);
-            System.out.println(second);
-        } else {
-            System.out.println(0);
-        }
+        first += second;
+        second = first - second;
+        first = first - second;
+        System.out.println(first);
+        System.out.println(second);
     }
 
     public static void main(String[] args) {
